33aae75d194038558f9c637fb28238b5a1b6bc87
AntonioSalazar <antonio.salazar.emrsn@gmail.com>
2019-03-23 16:10:00 -0600

    working in the details of the news

--- src/components/NewsOptions.js
+++ src/components/NewsOptions.js
@@ -8,9 +8,9 @@ class newsOptions extends React.Component {
       <div >
         <Nav className='newsOptions'>
           <Link to='news/business'><p>Negocios</p></Link> 
-          <Link to='/politics'><p>Politica</p></Link> 
-          <Link to='/security'><p>Seguridad</p></Link> 
-          <Link to='/sports'> <p>Deporte</p> </Link> 
+          <Link to='news/technology'><p>Tecnologia</p></Link> 
+          <Link to='news/security'><p>Seguridad</p></Link> 
+          <Link to='news/sports'> <p>Deporte</p> </Link> 
           <Link disabled to='/articles'> <p>Nota Roja</p> </Link>
         </Nav>
       </div>
--- src/index.js
+++ src/index.js
@@ -4,14 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
-<<<<<<< HEAD
 import {BrowserRouter as Router} from "react-router-dom"
 
-=======
-import {BrowserRouter as Router} from 'react-router-dom'
-
-
->>>>>>> 7630d5c6a7a792968ba6d89ba946c928edc16e52
 ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
